50bf68f8331f2dbd0166c0eb519e47992307fa80
4thel00z <4thel00z@gmail.com>
2018-02-01 20:16:41 +0100

    Remove exception silencing. Fix out param

--- domain_checker.py
+++ domain_checker.py
@@ -9,15 +9,17 @@ import aiohttp
 QUERY_URL = "https://api.domainsdb.info/search?query={query}"
 
 
-async def _get_similiar_domains(*, query: str, loop):
-    with aiohttp.ClientSession() as session:
+async def _get_similiar_domains(*, options: argparse.Namespace, query: str, loop: asyncio.AbstractEventLoop):
+    with aiohttp.ClientSession(loop=loop) as session:
         async with  session.get(QUERY_URL.format(query=query)) as response:
+            if options.out:
+                return await  response.content.read()
             return json.loads((await  response.content.read()).decode("UTF-8"))
 
 
-def get_similiar_domains(*, query, loop):
-    payload = loop.run_until_complete(_get_similiar_domains(query=query, loop=loop))
-    return payload["domains"] if "domains" in payload else payload
+def get_similiar_domains(*, options: argparse.Namespace, query: str, loop: asyncio.AbstractEventLoop):
+    payload = loop.run_until_complete(_get_similiar_domains(options=options, query=query, loop=loop))
+    return payload["domains"] if not options.out and "domains" in payload   else payload
 
 
 if __name__ == '__main__':
@@ -34,10 +36,7 @@ if __name__ == '__main__':
     options = ARGS.parse_args()
     asyncio.set_event_loop(None)
     loop: asyncio.AbstractEventLoop = asyncio.new_event_loop()
-    try:
-        result = get_similiar_domains(query=options.query.pop(), loop=loop)
-    except:
-        sys.exit(1)
+    result = get_similiar_domains(options=options, query=options.query.pop(), loop=loop)
 
     if options.print:
         pprint.pprint(result)
